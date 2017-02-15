8e7a11802269cb1dbf3c608a1fa169d7aa23a560
crstnbr <mail@carstenbauer.eu>
2017-02-15 10:15:15 +0100

    added compare() and compare_full()

--- src/math.jl
+++ src/math.jl
@@ -24,12 +24,71 @@ function reldiff(A::AbstractArray, B::AbstractArray)
 end
 export reldiff
 
+
 """
     absdiff(A, B)
 
 Difference of absolute values of `A` and `B`.
 """
-function absdiff(A::Array{Complex{Float64},2}, B::Array{Complex{Float64},2})
+function absdiff(A::AbstractArray, B::AbstractArray)
   return abs(A-B)
 end
 export absdiff
+
+
+"""
+    compare(A, B)
+
+Compares two matrices `A` and `B`, prints out the maximal absolute and relative differences
+and returns a boolean indicating wether `isapprox(A,B)`.
+"""
+function compare(A::AbstractArray, B::AbstractArray)
+  @printf("max absdiff: %.1e\n", maximum(absdiff(A,B)))
+  @printf("mean absdiff: %.1e\n", mean(absdiff(A,B)))
+  @printf("max reldiff: %.1e\n", maximum(reldiff(A,B)))
+  @printf("mean reldiff: %.1e\n", mean(reldiff(A,B)))
+  return isapprox(A,B)
+end
+export compare
+
+
+"""
+    compare_full(A, B)
+
+Compares two matrices `A` and `B`, prints out all absolute and relative differences
+and returns a boolean indicating wether `isapprox(A,B)`.
+"""
+function compare_full(A::AbstractArray, B::AbstractArray)
+  compare(A,B)
+  println("")
+  println("absdiff: ")
+  display(absdiff(A,B))
+  println("")
+  println("reldiff: ")
+  display(reldiff(A,B))
+  println("")
+  return isapprox(A,B)
+end
+export compare_full
+
+
+"""
+    comm(A, B)
+
+Commutator `AB - BA`.
+"""
+function comm(A::AbstractArray, B::AbstractArray)
+  return A*B - B*A
+end
+export comm
+
+
+"""
+    docommute(A, B)
+
+Checks if the matrices do (approx.) commute.
+"""
+function docommute(A::AbstractArray, B::AbstractArray)
+  return isapprox(comm(A,B),zeros(size(A)...))
+end
+export docommute
