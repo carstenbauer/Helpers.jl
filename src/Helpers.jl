ddc96535cf29ab70741ba8de58877f5a4c61856c
crstnbr <mail@carstenbauer.eu>
2017-12-01 17:44:34 +0100

    MOST_USED_PKGS

--- src/Helpers.jl
+++ src/Helpers.jl
@@ -50,15 +50,23 @@ export swap_cols!
 end # module
 
 
-"""
-    precompile_packages()
+const MOST_USED_PKGS = ["HDF5", "JLD", "PyPlot", "Distributions", "IJulia",
+     "DataFrames", "LsqFit", "Polynomials", "BenchmarkTools", "NPZ", "ProfileView",
+     "LaTeXStrings", "StatsBase", "Git", "LightXML", "Iterators", "CSV"]
 
-Trigger precompilation of all packages.
 """
-function precompile_packages()
+    precompile_packages(;all=false)
+
+Trigger precompilation of most used (or [all]) packages.
+"""
+function precompile_packages(; all=false)
     excl = ["Helpers", "ErrorAnalysis", "Yoni"]
     for pkg in keys(Pkg.installed())
         if pkg in excl
+            continue
+        end
+
+        if !all && !(pkg in MOST_USED_PKGS)
             continue
         end
 
@@ -75,15 +83,34 @@ export precompile_packages
 
 
 """
-    upgrade(build=false)
+    upgrade(;build=false, all=false)
 
-Upgrade all installed Julia packages (i.e. update, [build], and precompile).
+Upgrade most used (or [all]) installed Julia packages (i.e. update, [build], and precompile).
 """
-function upgrade(build=false)
+function upgrade(; build=false, all=false)
     Pkg.update();
     if build
         Pkg.build();
     end
-    precompile_packages()
+    precompile_packages(all=all)
 end
 export upgrade
+
+
+"""
+    install_most_used_pkgs()
+
+Upgrade all installed Julia packages (i.e. update, [build], and precompile).
+"""
+function install_most_used_pkgs()
+    for pkg in MOST_USED_PKGS
+        try
+            info("Installing: $pkg")
+            Pkg.add(pkg)
+        catch err
+            warn("Unable to install: $pkg")
+            warn(err)
+        end
+    end
+end
+export install_most_used_pkgs
