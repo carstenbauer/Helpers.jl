bdc33c1f4bccdcb4ae1efedcc234701ecad9e092
autodocs <autodocs>
2017-12-01 22:45:48 +0000

    build based on d211bbb

---
+++ latest/search_index.js
@@ -0,0 +1,83 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Helpers.jl Documentation",
+    "title": "Helpers.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Helpers.jl-Documentation-1",
+    "page": "Helpers.jl Documentation",
+    "title": "Helpers.jl Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Helpers.setrng-Tuple{MersenneTwister}",
+    "page": "Helpers.jl Documentation",
+    "title": "Helpers.setrng",
+    "category": "Method",
+    "text": "setrng(rng)\n\nReplaces current Base.Random.GLOBAL_RNG with rng.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Functions-1",
+    "page": "Helpers.jl Documentation",
+    "title": "Functions",
+    "category": "section",
+    "text": "setrng(rng::MersenneTwister)link to Helpers.jl Documentation\nlink to setrng(rng::MersenneTwister)"
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "Helpers.jl Documentation",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#",
+    "page": "Page title",
+    "title": "Page title",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Helpers.jl-Documentation-1",
+    "page": "Page title",
+    "title": "Helpers.jl Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Helpers.setrng-Tuple{MersenneTwister}",
+    "page": "Page title",
+    "title": "Helpers.setrng",
+    "category": "Method",
+    "text": "setrng(rng)\n\nReplaces current Base.Random.GLOBAL_RNG with rng.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Functions-1",
+    "page": "Page title",
+    "title": "Functions",
+    "category": "section",
+    "text": "setrng(rng::MersenneTwister)link to Helpers.jl Documentation\nlink to setrng(rng::MersenneTwister)"
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "Page title",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}
